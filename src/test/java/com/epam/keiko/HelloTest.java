60fc86e8dcd6ea76c86efc8dfb3d1491fcfd7455
Mihail Keiko <Mikhail_Keiko@epam.com>
2021-10-28 23:09:58 +0300

    add maven framework

---
+++ src/test/java/com/epam/keiko/HelloTest.java
@@ -0,0 +1,14 @@
+package com.epam.keiko;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloTest {
+
+
+    @Test
+    public void exampleTest() {
+        Assert.assertTrue(Boolean.TRUE);
+    }
+
+}
