e92543102171900fcdbbfcef54133361039941e0
Mihail Keiko <Mikhail_Keiko@epam.com>
2021-10-29 04:45:40 +0300

    Default test

---
+++ src/test/java/com/epam/keiko/DefaultTest.java
@@ -0,0 +1,13 @@
+package com.epam.keiko;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DefaultTest {
+
+    @Test
+    public void defaultTest() {
+        Assert.assertTrue("hello".equals("world"));
+    }
+
+}
